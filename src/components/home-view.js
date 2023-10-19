5af8c121e8d0ae32358f5bb32803e6e761475e2a
liztheshiz <65947305+liztheshiz@users.noreply.github.com>
2023-10-18 21:26:56 -0400

    added description and minor reformatting

--- src/components/home-view.js
+++ src/components/home-view.js
@@ -1,8 +1,14 @@
 function HomeView() {
     return (
         <div className="uk-container uk-container-small">
-            <h1 className="uk-heading-large uk-margin-top">Cat Calc</h1>
-            <form className="uk-form-horizontal uk-margin-large">
+            <h1 className="uk-heading-large uk-margin-large-top">Cat Calc</h1>
+            <p className="uk-margin">
+                Do you need to re-assess pussy portion sizes? Have you been dumping food into your cat's
+                bowl and letting it choose how much it wants to eat and now it's fat? Is your feline
+                friend simply watching its figure? You are in the right place! Enter your cat's profile
+                and weight to calculate its ideal daily calories.
+            </p>
+            <form className="uk-form-horizontal uk-margin-large cat-form">
                 <div className="uk-margin">
                     <label className="uk-form-label" for="cat-types">Select a cat profile:</label>
                     <div className="uk-form-controls">
@@ -19,7 +25,7 @@ function HomeView() {
                     <input className="uk-input uk-form-width-small" id="cat-weight" type="number"></input>
                 </div>
             </form>
-            <button class="uk-button uk-button-secondary">Calculate</button>
+            <button form="cat-form" class="uk-button uk-button-secondary">Calculate</button>
         </div>
     );
 }
