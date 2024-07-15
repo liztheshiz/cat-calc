22444f4db04ab22abdc45c9b45612b2af8c60f83
liztheshiz <65947305+liztheshiz@users.noreply.github.com>
2024-07-15 14:46:47 -0600

    footer link size and color updated to be more prominent

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,8 +1,8 @@
 function Footer({ currentView, setCurrentView }) {
     return (
         <footer className="footer">
-            {currentView == 'home' && <a className="uk-link-muted uk-text-small" onClick={() => setCurrentView('about')}>About</a>}
-            {currentView == 'about' && <a className="uk-link-muted uk-text-small" onClick={() => setCurrentView('home')}>Home</a>}
+            {currentView == 'home' && <a className="uk-text-medium" onClick={() => setCurrentView('about')}>About</a>}
+            {currentView == 'about' && <a className="uk-text-medium" onClick={() => setCurrentView('home')}>&lt; Home</a>}
         </footer>
     );
 }
